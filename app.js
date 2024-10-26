8192c20689d4f2c2bfb563c92c93058b40f4097f
Roland Ibiwoye <contact@rolandayo.com>
2024-10-26 23:34:09 +0100

    use strict update js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let listProductHTML = document.querySelector('.listProduct');
 let listCartHTML = document.querySelector('.listCart');
 let iconCart = document.querySelector('.icon-cart');
